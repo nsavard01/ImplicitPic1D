4b32c99f2cb1a3f2b99fcb0edf5e910a4d10f766
Nicolas <nicolas@d-pace.com>
2022-11-25 15:05:36 -0800

    Add constants module

---
+++ src/constants.f90
@@ -0,0 +1,15 @@
+module constants
+    use iso_fortran_env, only: real64
+    implicit none
+
+    
+    real(real64), parameter :: c = 299792458.0 ! m/s
+    real(real64), parameter :: eps_0 = 8.8541878128e-12 !F/m
+    real(real64), parameter :: m_p = 1.67262192369e-27 ! kg
+    real(real64), parameter :: m_e = 9.1093837015e-31 ! kg
+    real(real64), parameter :: k_B = 1.380649e-23 ! m^2 kg s^-2 K^-1
+    real(real64), parameter :: e = 1.602176634e-19 ! C
+    real(real64), parameter :: mu_0 = 1.25663706212e-6 ! m kg s^-2 A^-2
+
+
+end module constants
