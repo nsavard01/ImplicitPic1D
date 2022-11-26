c7393d5c5518ee18288bdc1f5b2bea048ea03c02
Nicolas <nicolas@d-pace.com>
2022-11-25 16:05:06 -0800

    Primary program can print correct integers of initial parameters

---
+++ src/BoundPlasmaExample.f90
@@ -0,0 +1,14 @@
+program BoundPlasmaExample
+    use constants
+    use iso_fortran_env, only: int32, real64
+    implicit none
+
+    integer(int32) :: num_grid_nodes, numParticles, maxIter
+    num_grid_nodes = 32
+    numParticles = 10000
+    maxIter = 50
+
+    print *, num_grid_nodes
+    
+
+end program BoundPlasmaExample
