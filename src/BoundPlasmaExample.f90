c6593581114d8b6826ef67c5380e7e93f1c21f6d
Nicolas <nicolas@d-pace.com>
2022-11-29 13:08:49 -0800

    Arrays of particle object works fine

--- src/BoundPlasmaExample.f90
+++ src/BoundPlasmaExample.f90
@@ -5,20 +5,27 @@ program BoundPlasmaExample
     use mod_particle
     implicit none
 
-    integer(int32) :: particleIdxFactor = 2
+    integer(int32) :: particleIdxFactor = 2, i
     integer(int32), parameter :: num_grid_nodes = 32, numParticles = 10000, maxIter = 50
     real(real64), parameter :: L_domain = 0.1, del_l = 0.005
-    real(real64) :: w_p = 1.0, n_ave = 5e14
+    real(real64) :: w_p = 1.0 !, n_ave = 5e14
     !real(real64) :: n_i = 5.0e14, T_e = 5.0, T_i = 0.025
     type(Domain) :: world
     type(Particle) :: electron
+    type(Particle) :: ion
+    type(Particle) :: particleList(2)
+
 
     world = Domain(num_grid_nodes)
     call world % constructSineGrid(del_l, L_domain)
 
-    electron = Particle(mass = m_p, q = e, w_p = w_p, N_p = numParticles, finalIdx = numParticles * particleIdxFactor)
-    call electron % initialize_n_ave(n_ave = n_ave, L_domain = L_domain)
-    
+    electron = Particle(mass = m_e, q = -e, w_p = w_p, N_p = numParticles, finalIdx = numParticles * particleIdxFactor)
+    ion = Particle(mass = m_p, q = e, w_p = w_p, N_p = numParticles, finalIdx = numParticles * particleIdxFactor)
+    particleList = [electron, ion]
+    do i = 1, 2
+        print *, particleList(i) % mass
+    end do
+
 
     
 
