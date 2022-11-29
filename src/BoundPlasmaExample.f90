5733b5bfbde032470443bf3788fa11cc1612c45e
Nicolas <nicolas@d-pace.com>
2022-11-28 18:07:33 -0800

    Particle with constructor working

--- src/BoundPlasmaExample.f90
+++ src/BoundPlasmaExample.f90
@@ -5,13 +5,24 @@ program BoundPlasmaExample
     use mod_particle
     implicit none
 
-    !integer(int32) :: i
+    integer(int32) :: particleSizeIncrease 
     integer(int32), parameter :: num_grid_nodes = 32, numParticles = 10000, maxIter = 50
     real(real64), parameter :: L_domain = 0.1, del_l = 0.005
     !real(real64) :: n_i = 5.0e14, T_e = 5.0, T_i = 0.025
     type(Domain) :: world
+    type(Particle) :: electron
+
     world = Domain(num_grid_nodes)
     call world % constructSineGrid(del_l, L_domain)
+
+    particleSizeIncrease = 2
+    electron = Particle(mass = m_p, q = e, w_p = 1, N_p = numParticles, finalIdx = numParticles*2)
+
+    print *, electron%q
+    print *, electron%mass
+    print *, electron%w_p
+    print *, electron%N_p
+    print *, electron%finalIdx
 
 
     
