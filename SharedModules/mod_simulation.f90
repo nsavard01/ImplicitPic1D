401f8b5720b99d73230e1fa879c049d7c84b0118
nsavard <nicolas@d-pace.com>
2023-03-17 20:51:00 +0000

    Also put mod_simulation as shared

--- SharedModules/mod_simulation.f90
+++ SharedModules/mod_simulation.f90
@@ -8,9 +8,10 @@ module mod_simulation
     use mod_domain
     use mod_potentialSolver
     use mod_collisions
+    use mod_NGP
 
     integer(int32) :: numTimeSteps, heatSkipSteps = 4
-    real(real64) :: del_t, simulationTime, numberCheckSteps
+    real(real64) :: del_t, simulationTime
 
 contains
 
@@ -145,127 +146,6 @@ contains
 
 
     end function readParticleInputs
-
-    ! ----------------------------- Initializations ---------------------------------------------
-
-    subroutine initialize_randUniform(part, L_domain, dx_dl, irand)
-        ! place particles randomly in each dx_dl based on portion of volume it take up
-        type(Particle), intent(in out) :: part
-        real(real64), intent(in) :: dx_dl(:), L_domain
-        integer(int32), intent(in out) :: irand
-        integer(int32) :: i, numInCell, idxLower, numPerCell(NumberXNodes-1)
-        real(real64) :: sumDxDl
-        idxLower = 1
-        sumDxDl = 0
-        do i=1, NumberXNodes-1
-            ! Use int to make sure always have a bit left over, otherwise will fill up before getting to end
-            numInCell = INT(part%N_p * dx_dl(i)/L_domain)
-            if (idxLower + numInCell > part % N_P + 1) then
-                stop "You are putting too many particles for the uniform particle case"
-            end if
-
-            
-            call getRandom(part%phaseSpace(1,idxLower:idxLower + numInCell-1), irand)
-            part%phaseSpace(1, idxLower:idxLower + numInCell - 1) = part%phaseSpace(1, idxLower:idxLower + numInCell - 1) + i
-            idxLower = idxLower + numInCell
-            numPerCell(i) = numInCell
-            
-        end do
-        if (idxLower < part%N_p + 1) then
-            call getRandom(part%phaseSpace(1, idxLower:part%N_p), irand)
-            part%phaseSpace(1, idxLower:part%N_p) = part%phaseSpace(1, idxLower:part%N_p) * (NumberXNodes - 1) + 1
-        end if
-        
-        
-    end subroutine initialize_randUniform
-
-    ! --------------------------- Diagnostics ------------------------------------
-
-    subroutine depositRhoDiag(rho, particleList, world) 
-        real(real64), intent(in out) :: rho(:)
-        type(Particle), intent(in) :: particleList(:)
-        type(Domain), intent(in) :: world
-        integer(int32) :: i, j, l_left
-        real(real64) :: d
-        rho = 0.0d0
-        do i=1, numberChargedParticles
-            do j = 1, particleList(i)%N_p
-                l_left = INT(particleList(i)%phaseSpace(1, j))
-                d = MOD(particleList(i)%phaseSpace(1, j), 1.0d0)
-                rho(l_left) = rho(l_left) + particleList(i)%q * particleList(i)%w_p * (1.0d0-d)
-                rho(l_left + 1) = rho(l_left + 1) + particleList(i)%q * particleList(i)%w_p * d
-            end do
-        end do
-        rho = rho / world%nodeVol
-        if (world%boundaryConditions(1) == 3) then
-            rho(1) = rho(1) + rho(NumberXNodes)
-            rho(NumberXNodes) = rho(1)
-        end if
-    end subroutine depositRhoDiag
-
-    subroutine loadParticleDensity(densities, particleList)
-        type(Particle), intent(in) :: particleList(:)
-        real(real64), intent(in out) :: densities(:,:)
-        integer(int32) :: i,j, l_left
-        real(real64) :: d
-        do i=1, numberChargedParticles
-            do j = 1, particleList(i)%N_p
-                l_left = INT(particleList(i)%phaseSpace(1,j))
-                d = MOD(particleList(i)%phaseSpace(1,j), 1.0d0)
-                densities(l_left, i) = densities(l_left, i) + particleList(i)%w_p * (1.0d0-d)
-                densities(l_left + 1, i) = densities(l_left + 1, i) + particleList(i)%w_p * d
-            end do
-        end do
-
-
-    end subroutine loadParticleDensity
-
-    subroutine WriteParticleDensity(densities, particleList, world, CurrentDiagStep, boolAverage) 
-        ! For diagnostics, deposit single particle density
-        ! Re-use rho array since it doesn't get used after first Poisson
-        real(real64), intent(in out) :: densities(:,:)
-        type(Particle), intent(in) :: particleList(:)
-        type(Domain), intent(in) :: world
-        integer(int32), intent(in) :: CurrentDiagStep
-        integer(int32) :: i
-        logical, intent(in) :: boolAverage
-        character(len=5) :: char_i
-        
-        do i=1, numberChargedParticles
-            densities(:,i) = densities(:,i)/world%nodeVol
-            if (world%boundaryConditions(1) == 3) then
-                densities(1,i) = densities(1,i) + densities(NumberXNodes, i)
-                densities(NumberXNodes, i) = densities(1, i)
-            end if
-            write(char_i, '(I3)'), CurrentDiagStep
-            if (boolAverage) then
-                open(41,file='../Data/Density/density_'//particleList(i)%name//"_Average.dat", form='UNFORMATTED')
-            else
-                open(41,file='../Data/Density/density_'//particleList(i)%name//"_"//trim(adjustl(char_i))//".dat", form='UNFORMATTED')
-            end if
-            write(41) densities(:,i)
-            close(41)
-        end do
-        
-    end subroutine WriteParticleDensity
-
-    subroutine writePhi(phi, CurrentDiagStep, boolAverage) 
-        ! For diagnostics, deposit single particle density
-        ! Re-use rho array since it doesn't get used after first Poisson
-        real(real64), intent(in) :: phi(:)
-        integer(int32), intent(in) :: CurrentDiagStep
-        character(len=5) :: char_i
-        logical, intent(in) :: boolAverage
-        write(char_i, '(I3)') CurrentDiagStep
-        if (boolAverage) then
-            open(41,file='../Data/Phi/phi_Average.dat', form='UNFORMATTED')
-        else
-            open(41,file='../Data/Phi/phi_'//trim(adjustl(char_i))//".dat", form='UNFORMATTED')
-        end if
-        write(41) phi
-        close(41)
-        
-    end subroutine writePhi
 
     
     ! -------------------------- Simulation ------------------------------------------
