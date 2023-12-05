4ae91ae2384d26201f0a0ddc3f74570f036ddeb9
nsavard <nicolas@d-pace.com>
2023-12-05 22:51:04 +0000

    Switch

--- OpenMP-BField/Libraries/Nitsol/nittfq__genmod.f90
+++ OpenMP-BField/Libraries/Nitsol/nittfq__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Dec  5 17:26:16 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Dec  5 22:03:40 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE NITTFQ__genmod
