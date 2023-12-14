4c82814528b8303bc92ad2a86d5ca225db0a95fd
nsavard <nicolas@d-pace.com>
2023-12-14 21:46:20 +0000

    switch back, maxwellian flux at wall doesn't work obviously

--- OpenMP-BField/Libraries/Nitsol/nitdrv__genmod.f90
+++ OpenMP-BField/Libraries/Nitsol/nitdrv__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Dec 14 19:35:08 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Dec 14 21:45:40 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE NITDRV__genmod
