e5c55813a4b5cf13c01e365bbdfae69cc2650be9
nsavard <nicolas@d-pace.com>
2024-01-27 01:13:04 +0000

    Working on ionization collision

--- OpenMP-BField/SharedModules/src/mod_NullCollision.f90
+++ OpenMP-BField/SharedModules/src/mod_NullCollision.f90
@@ -85,8 +85,8 @@ contains
         integer(int32), intent(in out) :: irand(numThread)
         real(real64), intent(in) :: del_t
         logical :: collisionBool
-        real(real64) :: P_null, numberSelectedReal, Rand, particleEnergy, targetVelocity(3), incidentVelocity(3), velocity_CM(3), energyCM, d_value, sigma_v, sigma_v_low
-        integer(int32) :: numberSelected, iThread, i, particleIndx, numberTotalParticles, indxHigh, indxLow, indxMiddle, collIdx
+        real(real64) :: P_null, numberSelectedReal, Rand, particleEnergy, targetVelocity(3), incidentVelocity(3), velocity_CM(3), energyCM, d_value, sigma_v, sigma_v_low, speedCM
+        integer(int32) :: numberSelected, iThread, i, particleIndx, numberTotalParticles, indxHigh, indxLow, indxMiddle, collIdx, addAmount
 
         P_null = 1.0d0 - EXP(-self%sigmaVMax * targetParticleList(self%reactantsIndx(2))%density * del_t)
         if (P_null > 0.5d0) then
@@ -95,7 +95,7 @@ contains
         end if
         
         !$OMP parallel private(iThread, i,numberSelected, numberSelectedReal, Rand, particleIndx, numberTotalParticles, targetVelocity, incidentVelocity, &
-            velocity_CM, energyCM, d_value, indxHigh, indxLow, indxMiddle, collIdx, sigma_v, sigma_v_low, collisionBool)
+            velocity_CM, energyCM, d_value, indxHigh, indxLow, indxMiddle, collIdx, sigma_v, sigma_v_low, collisionBool, speedCM, addAmount)
         iThread = omp_get_thread_num() + 1
         numberTotalParticles = particleList(self%reactantsIndx(1))%N_p(iThread)
         numberSelectedReal = P_null * real(numberTotalParticles)
@@ -108,6 +108,7 @@ contains
             incidentVelocity = particleList(self%reactantsIndx(1))%phaseSpace(2:4, particleIndx, iThread)
             targetVelocity = targetParticleList(self%reactantsIndx(2))%generate3DMaxwellianVelocity(irand(iThread))
             velocity_CM = incidentVelocity - targetVelocity
+            speedCM = SQRT(SUM(velocity_CM**2))
             energyCM = SUM(velocity_CM**2) * 0.5d0 * self%reducedMass / e !in eV
             indxLow = 1
             indxHigh = self%lengthArrays
@@ -136,89 +137,86 @@ contains
             Rand = ran2(irand(iThread))
             sigma_v_low = 0.0d0
             do collIdx = 1, self%numberCollisions
-                sigma_v = self%sigmaVArray(indxLow, collIdx) * (1.0d0 - d_value) + self%sigmaVArray(indxHigh, collIdx) * d_value + sigma_v_low
-                collisionBool = (Rand <= sigma_v/self%sigmaVMax)
-                if (collisionBool) then
-                    SELECT CASE (self%collisionType(collIdx))
-                    CASE(1)
-                        print *, 'elastic'
-                    CASE(2)
-                        print *, 'ionization'
-                    CASE(3)
-                        print *, 'excitation'
-                    CASE(4)
-                        print *, 'charge exchange'
-                    CASE(5)
-                        print *, 'dissociation'
-                    END SELECT
-                    exit
+                if (energyCM > self%energyThreshold(collIdx)) then
+                    sigma_v = self%sigmaVArray(indxLow, collIdx) * (1.0d0 - d_value) + self%sigmaVArray(indxHigh, collIdx) * d_value + sigma_v_low
+                    collisionBool = (Rand <= sigma_v/self%sigmaVMax)
+                    if (collisionBool) then
+                        SELECT CASE (self%collisionType(collIdx))
+                        CASE(1)
+                            print *, 'elastic'
+                        CASE(2)
+                            print *, 'ionization'
+                        CASE(3)
+                            print *, 'excitation'
+                        CASE(4)
+                            print *, 'charge exchange'
+                        CASE(5)
+                            print *, 'dissociation'
+                        END SELECT
+                        exit
+                    end if
+                    sigma_v_low = sigma_v
                 end if
-                sigma_v_low = sigma_v
             end do
             if (.not. collisionBool) then
                 print *, 'null collision'
             end if
         end do
         !$OMP end parallel
+        print *, ''
 
 
     end subroutine generateCollision
 
-    ! subroutine ionizationCollisionIsotropic(electron, ion, n_g, sigma, del_t, E_thres, T, irand)
-    !     ! Temporary subroutine for artificial ionization, using MCC, energy units in eV
-    !     ! T in eV
-    !     ! divide energy among electrons, isotropic scatter
-    !     ! For moment assume null collision with gas temperature = 0
-    !     type(Particle), intent(in out) :: electron, ion
-    !     real(real64), intent(in) :: n_g, sigma, del_t, E_thres, T
-    !     integer(int32), intent(in out) :: irand
-    !     real(real64) :: electronSpeed, Pcoll, R, speedPerParticle, phi, theta, e_vector(3), V_cm(3), delE, V_neutral(3), mu, mass_neutral, sumMassInverse
-    !     integer(int32) :: i, addIdx
-    !     addIdx = 0
-    !     mass_neutral = ion%mass + m_e
-    !     sumMassInverse = (2.0d0/m_e + 1.0d0/ion%mass)
-    !     mu = (m_e * mass_neutral)/ (m_e + mass_neutral)
-    !     do i=1, electron%N_p
-    !         call get3DMaxwellianVelocity(V_neutral, mass_neutral, T, irand)
-    !         delE = 0.5d0 * mu * SUM((electron%phaseSpace(2:4, i) - V_neutral)**2) - E_thres*e
-    !         if (delE > 0) then
-    !             R = ran2(irand)
-    !             electronSpeed = SQRT(SUM((electron%phaseSpace(2:4, i) - V_neutral)**2))
-    !             Pcoll = 1.0d0 - EXP(-n_g * sigma * del_t * electronSpeed)
-    !             if (Pcoll > R) then
-    !                 addIdx = addIdx + 1
-    !                 V_cm = (m_e *electron%phaseSpace(2:4, i) + V_neutral * mass_neutral) / (mass_neutral + m_e)
+    subroutine ionizationCollisionIsotropic(self, electron, ion, targetParticle, del_t, irand, energyCM, E_thres, speedCM, incidentVelocity, targetVelocity, velocityCM)
+        ! Temporary subroutine for artificial ionization, using MCC, energy units in eV
+        ! T in eV
+        ! divide energy among electrons, isotropic scatter
+        ! For moment assume null collision with gas temperature = 0
+        class(nullCollision), intent(in) :: self
+        type(Particle), intent(in out) :: electron, ion
+        type(targetParticle), intent(in) :: targetParticle
+        real(real64), intent(in) :: del_t, energyCM, E_thres, speedCM
+        real(real64), intent(in out) :: incidentVelocity(3), targetVelocity(3), velocityCM(3)
+        integer(int32), intent(in) :: partIndx, iThread
+        integer(int32), intent(in out) :: irand, addAmount
+        real(real64) :: speedPerParticle, phi, theta, e_vector(3), V_cm(3), delE, V_neutral(3), sumMassInverse, cos_theta, sin_theta, cos_phi, sin_phi, secTheta, thirdTheta
+        integer(int32) :: i, addIdx
+        
+        sumMassInverse = (2.0d0/m_e + 1.0d0/ion%mass)
+        delE = energyCM - E_thres
+        
+        V_cm = (m_e * incidentVelocity + targetVelocity * targetParticle%mass) / (targetParticle%mass + m_e)
 
-    !                 ! first add to electron
-    !                 theta = ACOS(1.0d0 - 2.0d0*ran2(irand))
-    !                 phi = ran2(irand) * 2 * pi
-    !                 e_vector(1) = COS(theta)
-    !                 e_vector(2) = SIN(phi) * SIN(theta)
-    !                 e_vector(3) = COS(phi) * SIN(theta)
-    !                 speedPerParticle = SQRT(2.0d0 * delE / sumMassInverse / m_e**2)
-    !                 electron%phaseSpace(2:4, i) = e_vector * speedPerParticle + V_cm
+        ! first add to electron
+        theta = ACOS(1.0d0 - 2.0d0*ran2(irand))
+        phi = ran2(irand) * 2 * pi
+        cos_theta = COS(theta)
+        sin_theta = SIN(theta)
+        cos_phi = COS(phi)
+        sin_phi = SIN(phi)
+        e_vector(1) = cos_theta
+        e_vector(2) = sin_phi * sin_theta
+        e_vector(3) = cos_phi * sin_theta
+        speedPerParticle = SQRT(2.0d0 * delE / sumMassInverse / m_e**2)
+        incidentVelocity = e_vector * speedPerParticle + V_cm
 
-    !                 ! second electron
-    !                 e_vector(1) = COS(theta + 2.0d0 * pi / 3.0d0)
-    !                 e_vector(2) = SIN(phi) * SIN(theta + 2.0d0 * pi / 3.0d0)
-    !                 e_vector(3) = COS(phi) * SIN(theta + 2.0d0 * pi / 3.0d0)
-    !                 electron%phaseSpace(2:4, electron%N_p + addIdx) = e_vector * speedPerParticle + V_cm
-    !                 electron%phaseSpace(1,electron%N_p + addIdx) = electron%phaseSpace(1,i)
+        ! second electron
+        secTheta = theta + 2.0d0 * pi/3.0d0
+        e_vector(1) = COS(secTheta)
+        e_vector(2) = sin_phi * SIN(secTheta)
+        e_vector(3) = cos_phi * SIN(secTheta)
+        velocityCM = e_vector * speedPerParticle + V_cm
 
-    !                 ! ion
-    !                 e_vector(1) = COS(theta + 4.0d0 * pi / 3.0d0)
-    !                 e_vector(2) = SIN(phi) * SIN(theta + 4.0d0 * pi / 3.0d0)
-    !                 e_vector(3) = COS(phi) * SIN(theta + 4.0d0 * pi / 3.0d0)
-    !                 speedPerParticle = SQRT(2.0d0 * delE / sumMassInverse / ion%mass**2)
-    !                 ion%phaseSpace(2:4, ion%N_p + addIdx) = e_vector * speedPerParticle + V_cm
-    !                 ion%phaseSpace(1,ion%N_p + addIdx) = electron%phaseSpace(1,i)
-    !             end if
-    !         end if
-    !     end do
-    !     electron%N_p = electron%N_p + addIdx
-    !     ion%N_p = ion%N_p + addIdx
-    !     inelasticEnergyLoss = inelasticEnergyLoss + e*E_thres * addIdx * electron%w_p
-    ! end subroutine ionizationCollisionIsotropic
+        ! ion
+        thirdTheta = theta + 4.0d0 * pi / 3.0d0
+        e_vector(1) = COS(thirdTheta)
+        e_vector(2) = sin_phi * SIN(thirdTheta)
+        e_vector(3) = cos_phi * SIN(thirdTheta)
+        speedPerParticle = SQRT(2.0d0 * delE / sumMassInverse / ion%mass**2)
+        targetVelocity = e_vector * speedPerParticle + V_cm
+           
+    end subroutine ionizationCollisionIsotropic
 
 
 end module mod_NullCollision
